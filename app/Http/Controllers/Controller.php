3a3c7076a1e170ef932d02baaef06e4a3fad05d8
guerramg <agenciaintuito@gmail.com>
2022-02-16 21:35:46 -0300

    token

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -12,6 +12,6 @@ class Controller extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
     public function token(){
-        return 'kevin';
+        return '##############';
     }
 }
